979833bdbd1319a909abc4cc234d4689ef16e55c
Along0426 <2798902906@qq.com>
2022-09-29 11:04:39 +0800

    feat(custom): 配置跨域

--- vue.config.js
+++ vue.config.js
@@ -3,6 +3,15 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 9527,
-    host: "localhost"
+    host: "localhost",
+    proxy: {
+      [process.env.VUE_APP_BASE_API]: {
+        target: process.env.VUE_APP_URL,
+        changeOrigin: true,
+        pathRewrite: {
+          ["^" + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    }
   }
 })
